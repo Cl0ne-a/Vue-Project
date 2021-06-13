0a473f0801518499402d325db4713068519e8bc2
anna <unit10@protonmail.com>
2021-06-13 15:43:01 +0300

    refactored v-on, v-bind

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const App = {
             placeholderString: 'Input note Title',
             title: 'Notes List',
             inputVal: '',
-            notes: ['some note', 225, 'note 54']
+            notes: ['some note', 569, 'note 54']
         }
     },
     methods: {
@@ -13,17 +13,20 @@ const App = {
         },
 
         addNewNote() {
-            this.notes.push(this.inputVal)
+            if (this.inputVal !== '') {
+                this.notes.push(this.inputVal)
+                this.inputVal = ''
+            }
+        },
+
+        toUpRegister(item) {
+            return typeof this.item === 'number' ? typeof this.item : JSON.stringify(item).toUpperCase();
         },
 
         removeNote(idx, event) {
             this.notes.splice(idx, 1)
-
         }
     }
-    
-    
-    
 }
 
 Vue.createApp(App).mount('#app')
